089f6d64d21cbabb3790316fa5d6d3a2d8fe24db
Svetlana Vassileva <72747194+svetlilaloli@users.noreply.github.com>
2021-02-11 11:35:02 +0000

    Add files via upload

---
+++ CupsAndBottles.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CupsAndBottles
+{
+    class CupsAndBottles
+    {
+        static void Main()
+        {
+            Queue<int> cups = new Queue<int>(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
+            Stack<int> bottles = new Stack<int>(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
+            int wastedWater = 0;
+            int cup;
+            int bottle;
+
+            while (cups.Count > 0 && bottles.Count > 0)
+            {
+                cup = cups.Dequeue();
+                bottle = bottles.Pop();
+
+                while (cup > 0)
+                {
+                    cup -= bottle;
+                    if (cup <= 0)
+                    {
+                        break;
+                    }
+                    if (bottles.Count > 0)
+                    {
+                        bottle = bottles.Pop();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                wastedWater += Math.Abs(cup);
+            }
+            if (bottles.Count > 0)
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", bottles)}");
+            }
+            else if (cups.Count > 0)
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+            }
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        }
+    }
+}
